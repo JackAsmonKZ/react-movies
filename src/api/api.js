f72c01379828cce35267f9604eaeabd4626b9c0d
JackAsmonKZ <warzoneasmon@gmail.com>
2025-01-15 16:03:25 +0500

    fix: api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 
 export const fetchMovies = async (query, type = "all") => {
-  const filmType = type === "all" ? "" : `${type}`;
+  const filmType = type === "all" ? "" : `&type=${type}`;
   try {
     const response = await fetch(
-      `${BASE_URL}?apikey=${API_KEY}&s=${query}&type=${filmType}`
+      `${BASE_URL}?apikey=${API_KEY}&s=${query}${filmType}`
     );
     const data = await response.json();
     return data;
